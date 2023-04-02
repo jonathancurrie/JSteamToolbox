f696679f706cf08ab417456ad2f69c97a1b6044a
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2023-04-03 11:49:47 +1200

    Added programmatic install

--- JSteam_Install.m
+++ JSteam_Install.m
@@ -1,10 +1,25 @@
-function JSteam_Install
-%% Installation File for JSteam
+function JSteam_Install(savePath,runTests,openBrowser)
+% JSteam Toolbox Installation File
+%
+%  JSteam_Install(savePath, runTests, openBrowser)
+%
+%   savePath: Save the paths added by JSteam to the MATLAB path 
+%   runTests: Run the post-installation tests 
+%   openBrowser: Whether to open the JSteam Toolbox Website after installation 
+%
+% All arguments are optional and if not supplied, the user will be prompted
+% to enter their selection in the MATLAB Command Window. True is the
+% default option for each argument.
+%
+% You MUST be in the current directory of this file!
+%
+%   Copyright (C) 2023 Jonathan Currie (Control Engineering)
+%   https://www.controlengineering.co.nz/Wikis/JSteam/pmwiki.php/Matlab/Main
 
-% In order to run this tool, please run this file to setup the required
-% directories. You MUST be in the current directory of this file!
-
-%   Copyright (C) 2012 Jonathan Currie (I2C2)
+% Handle missing input args
+if (nargin < 3), openBrowser = []; end
+if (nargin < 2), runTests = []; end
+if (nargin < 1), savePath = []; end
 
 cpath = cd;
 try
@@ -60,7 +75,11 @@ genp(ind == 1) = [];
 addpath(genp{:});
 rehash
 fprintf('Done\n\n');
-in = input('- Would You Like To Save the Path Changes? (Recommended) (y/n): ','s');
+if (isempty(savePath))
+    in = input('- Would You Like To Save the Path Changes? (Recommended) (y/n): ','s');
+else
+    in = bool2yn(savePath);
+end    
 if(strcmpi(in,'y'))
     try
         savepath;
@@ -72,7 +91,11 @@ if(strcmpi(in,'y'))
 end
 
 %Post Install Test if requested
-in = input('\n- Would You Like To Run Post Installation Tests? (Recommended) (y/n): ','s');
+if (isempty(runTests))
+    in = input('\n- Would You Like To Run Post Installation Tests? (Recommended) (y/n): ','s');
+else
+    in = bool2yn(runTests);
+end    
 if(strcmpi(in,'y'))
     ok = JSteam_Install_Test(1);
 else
@@ -80,8 +103,10 @@ else
 end
 
 if(ok)
-    %Launch Help Browser
-    web('https://www.controlengineering.co.nz/Wikis/JSteam/pmwiki.php/Matlab/Main','-helpbrowser');
+    %Launch Examples page
+    if (isempty(openBrowser) || (openBrowser == true))
+        web('https://www.controlengineering.co.nz/Wikis/JSteam/pmwiki.php/Matlab/Main','-browser');
+    end
 
     %Finished
     fprintf('\nJSteam Toolbox Installation Complete!\n');
@@ -249,4 +274,11 @@ if(missing)
     OK = false;
 else
     OK = true;
+end
+
+function in = bool2yn(val)
+if (isempty(val) || val == true)
+    in = 'y';
+else
+    in = 'n';
 end
